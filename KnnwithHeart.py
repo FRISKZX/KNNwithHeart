b285172542d9cdd97ee507b7bba0b3f70265704d
FRISKZX <phiphatphong2003g@gmail.com>
2025-04-09 14:34:37 +0700

    Update KnnwithHeart.py

--- KnnwithHeart.py
+++ KnnwithHeart.py
@@ -8,19 +8,15 @@ import matplotlib.pyplot as plt
 st.title('การจำแนกข้อมูลด้วยเทคนิค Machine Learning')
 st.image("./img/Stein_gate.jpg")
 
-col1, col2, col3 = st.columns(3)
+col1, col2 = st.columns(3)
 
 with col1:
    st.header("Versicolor")
-   st.image("./img/iris1.jpg")
+   st.image("./img/Bad_heart.png")
 
 with col2:
-   st.header("Verginiga")
-   st.image("./img/iris2.jpg")
-
-with col3:
-   st.header("Setosa")
-   st.image("./img/iris3.jpg")
+   st.header("การทำนายโรคหัวใจ")
+   st.image("./img/Good_heart.png")
 
    html_7 = """
 <div style="background-color:#c5f18a;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
